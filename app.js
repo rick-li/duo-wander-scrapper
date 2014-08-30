07da763bd553b7f5e43c7c28e5349e88e600ee9d
Rick Li <racke1983cn@gmail.com>
2014-08-30 08:55:26 +0800

    fix data for timer

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var server = require('http').createServer(app).listen(port);
 var jquery = fs.readFileSync("./node_modules/jquery/dist/jquery.js", "utf-8");
 
 var allCountries = ['Afghanistan','Albania','Algeria','American Samoa','Andorra','Angola','Anguilla','Antigua and Barbuda','Argentina','Armenia','Aruba','Australia','Austria','Azerbaijan','Bahamas','Bahrain','Bangladesh','Barbados','Belarus','Belgium','Belize','Benin','Bhutan','Bolivia','Bosnia Hercegovina','Botswana','Brazil','British Indian Ocean Territory','Brunei Darussalam','Bulgaria','Burkina Faso','Burundi','Cambodia','Cameroon','Canada','Cayman Islands','Central African Republic','Chad','Chile','China','Christmas Island','Cocos Islands','Colombia','Comoros','Congo','Cook Islands','Costa Rica','Croatia','Cuba','Cyprus','Czech Republic','Denmark','Djibouti','Dominica','Dominican Republic','East Timor','Ecuador','Egypt','El Salvador','Equatorial Guinea','Estonia','Ethiopia','Falkland Islands','Faroe Islands','Fiji','Finland','France','French Guiana','French Polynesia','French Southern Territories','Gambia','Georgia','Germany','Ghana','Gibraltar','Gobon','Great Britain(UK)','Greece','Greenland','Grenada','Guadeloupe','Guam','Guatemala','Guinea - Bissau','Guyana','Guynea','Haiti','Honduras','','Hungary','Iceland','India','Indonesia','Iran','Iraq','Ireland','Israel','Italy','Ivory Coast','Jamaica','Japan','Jordan','Kazakhstan','Kenya','Kiribati','Korea - North','Korea - South','Kuwait','Kyrgyzstan','Lao People \'s Republic','Latvia','Lebanon','Lesotho','Liberia','Libya','Liechtenstein','Lithuania','Luxembourg','Madagascar','Malawi','Malaysia','Maldives','Mali','Malta','Marshall Islands','Martinique','Mauritania','Mexico','Micronesia','Moldova','Monaco','Mongolia','Montserrat','Morocco','Mozambique','Myanmar','Namibia','Nauru','Nepal','Netherlands','Netherlands Antilles','New Caledonia','New Zealand','Nicaragua','Niger','Nigeria','Niue','Norfolk Island','Northern Mariana Islands','Norway','Oman','Pakistan','Palau','Panama','Papua New Guinea','Paraguay','Peru','Philippines','Pitcairn Island','Poland','Portugal','Puerto Rico','Qatar','Reunion Island','Romania','Russian Federation','Rwanda','Samoa','San Marino','Saudi Arabia','Senegal','Seychelles','Sierra Leone','Singapore','Slovakia','Solomon Islands','Somalia','South Africa','Spain','Sri Lanka','St. Helena','St. Lucia','Sudan','Suriname','Swaziland','Sweden','Switzerland','Syrian Arab Republic','Tajikistan','Tanzania','Thailand','Togo','tokelau','Tonga','Tunisia','Turkey','Turkmenistan','Tuvalu','Uganda','Ukrainian SSR','United Arab Emirates','United Kingdom','United States','Uruguay','Vanuatu','Vatican City State','Venezuela','Vietnam','Virgin Islands','Western Sahara','Yemen','Yugoslavia','Zaire','Zambia','Zimbabwe'];
-
+var data = [];
 var getCountries = function() {
 
       jsdom.env({
@@ -35,7 +35,7 @@ var getCountries = function() {
             console.log(finishedCountries)
             delete finishedCountries['China'];
             delete allCountries['China'];
-            allCountries = allCountries.map(function(c) {
+            data = allCountries.map(function(c) {
               console.log(finishedCountries[c]);
               var found = !!finishedCountries[c];
               var item = {name: c, found: found};
@@ -45,7 +45,7 @@ var getCountries = function() {
               return item;
             });
 
-            console.log(allCountries);
+            console.log(data);
         }
     });
 };
@@ -54,5 +54,5 @@ setInterval(getCountries, 60 * 60 * 1000);
 
 
 app.get('/countries', function(req, res, next) {
-  res.render('countries', {countries: allCountries});
+  res.render('countries', {countries: data});
 });
