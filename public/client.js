d0504666739218f2902ad29cc26083ccc5d7758e
Rick Li <racke1983cn@gmail.com>
2014-09-02 23:58:52 +0800

    encode

--- public/client.js
+++ public/client.js
@@ -14,8 +14,11 @@ $(document).ready(function() {
       }
       var url = '#';
       if (latlng) {
-        url = 'https://ditu.google.cn/maps/api/staticmap?center=' + latlng + '&zoom=18&size=800x800&maptype=satellite&markers=color:blue|' + latlng + '&scale=2';
+        var param = 'center=' + encodeURIComponent(latlng) + '&zoom=18&size=800x800&maptype=satellite&markers=color:blue|' + encodeURIComponent(latlng) + '&scale=2';
+
+        url = 'https://ditu.google.cn/maps/api/staticmap?'+  param;
       }
+      
       return '<tr><td>' + item.name + '</td><td class="location"><a href="' + url + '">' + location + '</a></td></tr>'
     }).get().join());
   }
