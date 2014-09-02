f04add7f7cfce8a9fddb4c062fbcb0d39c4dd41a
Rick Li <racke1983cn@gmail.com>
2014-09-02 23:50:18 +0800

    pop

--- public/client.js
+++ public/client.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
 
       var location = item.location ? item.location : '';
       var re = /(\d+°\d+.+\d+°.+\w)/
-      var text = $(this).text();
-      var matchs = text.match(re);
+      
+      var matchs = location.match(re);
       var latlng = '';
       if (matchs) {
         latlng = matchs[1];
@@ -40,35 +40,5 @@ $(document).ready(function() {
     return !c.found;
   }).length);
   $('#refreshedat').text(new Date(date));
-
-  $('table').on('click', '.location', function() {
-    var re = /(\d+°\d+.+\d+°.+\w)/
-    var text = $(this).text();
-    var matchs = text.match(re);
-    if (matchs) {
-      var location = matchs[1];
-      window.location = 'https://ditu.google.cn/maps/api/staticmap?center=' + location + '&zoom=18&size=800x800&maptype=satellite&markers=color:blue|' + location + '&scale=2';
-      //   //42.506795,1.522411
-      //   var mapOptions = {
-      //   center: new google.maps.LatLng(42.506795,1.522411),
-      //   zoom: 17,
-      //   height: 800,
-      //   width: 800
-      // };
-      // var map = new google.maps.Map($(".map-container").get(0),
-      //   mapOptions);
-    }
-
-  });
-
-
-  function initialize() {
-    var mapOptions = {
-      center: new google.maps.LatLng(-34.397, 150.644),
-      zoom: 8
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-      mapOptions);
-  }
 
 });
